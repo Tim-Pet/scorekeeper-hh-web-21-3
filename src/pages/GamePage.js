6dbf8c8560c58bb6331490be2119a8d2ed3b7371
Tim Petersen <tim.petersen1@outlook.de>
2021-05-21 10:52:58 +0200

    Adjust styles to new structure

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import Button from '../components/lowLevel/Button'
+import styled from 'styled-components'
 import Game from '../components/Game'
+import Button from '../components/lowLevel/Button'
 
 const GamePage = ({ game, updateScore, handleEndGame }) => {
   return (
     <>
       <Game game={game} onMinus={updateScore} onPlus={updateScore} />
-      <Button onClick={handleEndGame}>End game</Button>
+      <EndButton onClick={handleEndGame}>End game</EndButton>
     </>
   )
 }
 
 export default GamePage
+
+const EndButton = styled(Button)`
+  bottom: 10px;
+  left: 50%;
+  position: absolute;
+  transform: translate(-50%);
+  width: 80%;
+`
