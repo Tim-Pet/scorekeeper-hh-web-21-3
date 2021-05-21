d05598cf0482c3104e115d8365fbce7d2b382f0a
Tim Petersen <tim.petersen1@outlook.de>
2021-05-21 10:31:13 +0200

    Take Nav & Header out of pages

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
 import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import styled from 'styled-components'
 import PlayPage from './pages/PlayPage'
 import GamePage from './pages/GamePage'
 import HistoryPage from './pages/HistoryPage'
+import Navigation from './components/Navigation'
+import Header from './components/Header'
 
 function App() {
   const [game, setGame] = useState({})
   const [gameHistory, setGameHistory] = useState([])
   const [activePage, setActivePage] = useState('play')
 
+  const pages = [
+    { title: 'Play', id: 'play' },
+    { title: 'History', id: 'history' },
+  ]
+
   return (
-    <>
-      {activePage === 'play' && (
-        <PlayPage
-          handleGameSubmit={handleGameSubmit}
-          activePage={activePage}
-          handleNavigate={setActivePage}
+    <Container>
+      <Header>{activePage}</Header>
+      <Body>
+        {activePage === 'play' && (
+          <PlayPage
+            handleGameSubmit={handleGameSubmit}
+            activePage={activePage}
+            handleNavigate={setActivePage}
+          />
+        )}
+        {activePage === 'game' && (
+          <GamePage
+            game={game}
+            updateScore={updateScore}
+            handleEndGame={handleEndGame}
+          />
+        )}
+        {activePage === 'history' && (
+          <HistoryPage
+            gameHistory={gameHistory}
+            activePage={activePage}
+            handleNavigate={setActivePage}
+          />
+        )}
+      </Body>
+      {activePage !== 'game' && (
+        <Navigation
+          onNavigate={setActivePage}
+          pages={pages}
+          currentPageId={activePage}
         />
       )}
-      {activePage === 'game' && (
-        <GamePage
-          game={game}
-          updateScore={updateScore}
-          handleEndGame={handleEndGame}
-        />
-      )}
-      {activePage === 'history' && (
-        <HistoryPage
-          gameHistory={gameHistory}
-          activePage={activePage}
-          handleNavigate={setActivePage}
-        />
-      )}
-    </>
+    </Container>
   )
 
   function handleGameSubmit(gameName, playerNames) {
@@ -62,3 +80,15 @@ function App() {
 }
 
 export default App
+
+const Container = styled.div`
+  display: grid;
+  height: 100vh;
+  width: 100%;
+  grid-template-rows: 60px auto 60px;
+  position: fixed;
+`
+const Body = styled.section`
+  overflow-y: auto;
+  padding: 16px;
+`
--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,32 +1,14 @@
 import React from 'react'
-import styled from 'styled-components'
 import Button from '../components/lowLevel/Button'
 import Game from '../components/Game'
-import Header from '../components/Header'
 
 const GamePage = ({ game, updateScore, handleEndGame }) => {
   return (
-    <Container>
-      <Header>History</Header>
-      <Body>
-        <Game game={game} onMinus={updateScore} onPlus={updateScore} />
-      </Body>
+    <>
+      <Game game={game} onMinus={updateScore} onPlus={updateScore} />
       <Button onClick={handleEndGame}>End game</Button>
-    </Container>
+    </>
   )
 }
 
 export default GamePage
-
-const Container = styled.div`
-  display: grid;
-  grid-template-rows: 60px auto 60px;
-  height: 100vh;
-  width: 100vw;
-  position: fixed;
-`
-
-const Body = styled.section`
-  overflow-y: auto;
-  padding: 16px;
-`
